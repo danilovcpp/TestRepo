b084d33070733372b69a8a230722076d8b377322
danilovcpp <danilov.cpp@gmail.com>
2017-09-29 11:56:19 +0600

    Add simple model

--- TestBackend/App_Start/WebApiConfig.cs
+++ TestBackend/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace TestBackend
@@ -13,6 +14,9 @@ namespace TestBackend
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                  .Add(new MediaTypeHeaderValue("text/html"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
---
+++ TestBackend/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestBackend.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; set; }
+        public State State { get; set; }
+        
+        public int Project_Id { get; set; }
+    }
+}
